cfd1472da5a03aba9e1e4cb856b54192d5429e48
carry0987 <carry0987@users.noreply.github.com>
2018-08-21 16:56:57 +0800

    Fix set script

--- repo/set.sh
+++ repo/set.sh
@@ -7,6 +7,4 @@ wget https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Tools/tools.sh
 wget https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Rclone-Mount/rclone-mount.sh
 wget https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Rclone-Mount/delete-rclone-mount.sh
 
-rm set.sh
-
 exit 0
