880f750d6370ccd4b557956358f64d0b5971baaa
carry0987 <carry0987@users.noreply.github.com>
2018-08-21 16:55:26 +0800

    Update repo

--- repo/set.sh
+++ repo/set.sh
@@ -7,4 +7,6 @@ wget https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Tools/tools.sh
 wget https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Rclone-Mount/rclone-mount.sh
 wget https://raw.github.com/carry0987/Raspberry-Pi-Repo/master/Rclone-Mount/delete-rclone-mount.sh
 
+rm set.sh
+
 exit 0
