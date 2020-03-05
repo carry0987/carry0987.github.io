2927e1e173b62394020572fac46e9bce9387c70b
carry0987 <carry0987@users.noreply.github.com>
2020-03-05 21:08:11 +0800

    Update eh_shath_seller.user.js

--- script/eh_shath_seller.user.js
+++ script/eh_shath_seller.user.js
@@ -2,7 +2,7 @@
 // @name         EH Hath Seller
 // @author       carry0987
 // @namespace    https://github.com/carry0987
-// @version      1.0.0
+// @version      1.1.0
 // @description  Auto summary credit of hath
 // @icon         https://carry0987.github.io/favicon.png
 // @match        https://e-hentai.org/exchange.php?t=hath
@@ -28,73 +28,21 @@
         hathrate = 0,
         country = '';
 
-    //Max Speed
+    //Get value
+    var current_price = document.querySelector('.stuffbox>div>div:nth-child(2)>div>strong:nth-child(2)');
+    var get_price = current_price.nextSibling.wholeText;
+    get_price = get_price.split(' ')[1].replace(/\,/g, '');
+    get_price = parseInt(get_price);
     var ask_count = document.getElementById('ask_count');
     var ask_price = document.getElementById('ask_price');
-    console.log(ask_count.value);
-    if (ask_count.value > 1) {
-        maxSpeed = `${maxSpeed} KB/s`;
-    } else {
-        maxSpeed = `${Math.round(maxSpeed/100)/10} MB/s`;
-    }
-/*
-    //Hit Rate
-    let hitrates = document.querySelectorAll('.hct > tbody > tr > td:nth-child(13)');
-    for (let item of hitrates) {
-        try {
-            hitrate += parseFloat(item.textContent.match(/^[\d.]+/g));
-        } catch (e) {
-            throw new Error('Failed parsing hitrate');
+
+    let init = function() {
+        ask_count.onchange = function(e) {
+            var get_count = parseInt(ask_count.value);
+            if (get_count > 1) {
+                ask_price.value = get_price * get_count;
+            }
         }
     }
-    hitrate = `${hitrate} / min`;
-
-    //Hath Rate
-    let hathrates = document.querySelectorAll('.hct > tbody > tr > td:nth-child(14)');
-    for (let item of hathrates) {
-        try {
-            hathrate += parseFloat(item.textContent.match(/^[\d.]+/g));
-        } catch (e) {
-            throw new Error('Failed parsing hathrate');
-        }
-    }
-    hathrate = `${hathrate} / day`;
-
-
-    //Build row
-    let el = `<tr>
-    <td>${client}</td>
-    <td>${id}</td>
-    <td>${status}</td>
-    <td>${created}</td>
-    <td>${lastSeen}</td>
-    <td>${filesServed}</td>
-    <td>${clientIP}</td>
-    <td>${port}</td>
-    <td>${version}</td>
-    <td>${maxSpeed}</td>
-    <td>${trust}</td>
-    <td>${quality}</td>
-    <td>${hitrate}</td>
-    <td>${hathrate}</td>
-    <td>${country}</td>
-    </tr>`;
-
-    //Inject row
-    let lastrow = document.querySelector('.hct > tbody > tr:last-child');
-    lastrow.insertAdjacentHTML('afterend', el);
-
-    //Inject stylesheet
-    let stylesheet = `
-        .hct tr:last-child > td {
-            padding-top: 1px;
-            border-top: 1px solid #5C0D11;
-            background: none transparent !important;
-        }
-    `;
-
-    let stylesheetEl = document.createElement('style');
-    stylesheetEl.innerHTML = stylesheet;
-    document.body.appendChild(stylesheetEl);
-    */
+    init()
 })();
