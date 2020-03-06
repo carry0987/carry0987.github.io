c584b87b78ae1c6d5353da10bd1783f8e84a699d
carry0987 <carry0987@users.noreply.github.com>
2020-03-06 21:55:30 +0800

    Update hv_how_rich_am_i.user.js

--- script/hv_how_rich_am_i.user.js
+++ script/hv_how_rich_am_i.user.js
@@ -2,7 +2,7 @@
 // @name         HV How Rich Am I ?
 // @author       carry0987
 // @namespace    https://github.com/carry0987
-// @version      1.0.0
+// @version      1.1.0
 // @description  Show how many Credits you have
 // @icon         https://carry0987.github.io/favicon.png
 // @include      https://hentaiverse.org/*
@@ -25,14 +25,21 @@ var getCredit = function() {
     frm.width = frm.height = frm.frameBorder = 0
     frm.addEventListener('load', function() {
         var doc = this.contentDocument
-        var cell = document.getElementById('#networth')
-        var credit
-        credit = doc.body.innerHTML.match(/Available:\s*([,0-9]*)\s*kGP/i)[1]
-        credit = parseInt(credit.replace(/,/g, '')) * 1000
-        credit = credit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-        console.log('Credits: ' + cell)
-        //console.log('Credits: ' + cell)
-        //if (credit != '0') { cell.textContent = 'Credits: ' + credit }
+        var cell = doc.getElementById('networth')
+        var credit = cell.innerHTML.match(/Credits:\s*([,0-9]*)\s*/i)[1]
+        credit = parseInt(credit.replace(/,/g, ''))
+        //credit = credit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        var show_credit_1 = document.createElement('div')
+        show_credit_1.setAttribute('id', 'networth')
+        show_credit_1.setAttribute('width', '148px')
+        var show_credit_2 = document.createElement('div')
+        show_credit_2.className = 'fc4 fal fcb'
+        show_credit_2.setAttribute('width', '138px')
+        var credit_box = document.getElementById('mainpane')
+        if (credit_box !== null) {
+            credit_box.appendChild(show_credit_1)
+        }
+        if (credit != '0') { show_credit_1.innerHTML = 'Credits: ' + credit }
         this.parentElement.removeChild(this)
     }, false)
     doc.body.appendChild(frm)
