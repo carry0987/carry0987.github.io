37bb0dda65426d351fc66ed9f2634921f15e7373
carry0987 <carry0987@users.noreply.github.com>
2024-11-04 12:31:43 +0800

    Update repo.tsx

--- src/view/component/repo.tsx
+++ src/view/component/repo.tsx
@@ -3,7 +3,7 @@ import { RepoProps } from '@/type/repo';
 export const Repo = ({ name, description, language, html_url }: RepoProps) => {
     return (
         <div className="bg-white p-4 rounded-lg shadow-lg text-start">
-            <a className="font-bold text-blue-500 hover:underline" href={html_url} target="_blank">
+            <a className="font-bold text-blue-500 hover:underline" href={html_url} target={html_url.startsWith('http') ? '_blank' : '_self'}>
                 {name}
             </a>
             <div className="mt-2 mb-3 truncate hover:text-wrap">{description}</div>
