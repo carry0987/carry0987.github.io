951b750827abfb9a1c2661da038787cf21431bea
carry0987 <carry0987@users.noreply.github.com>
2025-11-23 13:25:06 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'app')
+        }
+    },
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
     build: {
         cssMinify: true,
