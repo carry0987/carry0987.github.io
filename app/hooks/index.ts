f8b4c1a7db50882af358cd8cf23acd6436c658fb
carry0987 <carry0987@users.noreply.github.com>
2025-12-14 03:44:00 +0800

    Update index.ts

--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -1,3 +1,4 @@
 export { usePageVisibility } from './usePageVisibility';
 export { useCameraAvailability } from './useCameraAvailability';
 export { useResponsiveScale } from './useResponsiveScale';
+export { useIsMobile } from './useIsMobile';
